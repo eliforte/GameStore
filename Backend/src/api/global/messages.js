53a27cbbc9ef369e460f4637d9fb6a76f2020eed
Elias Forte <eliasforte.dev@gmail.com>
2021-12-10 19:42:52 -0300

    messagens de erros do token implementada

---
+++ Backend/src/api/global/messages.js
@@ -0,0 +1,8 @@
+const { UNAUTHORIZED } = require('http-status-codes').StatusCodes;
+
+const JWT_MALFORMED_401 = { message: 'jwt malformed', status: UNAUTHORIZED };const MISSING_TOKEN_401 = { err: { message: 'missing auth token', status: UNAUTHORIZED } };
+
+module.exports = {
+  JWT_MALFORMED_401,
+  MISSING_TOKEN_401
+};
--- Backend/src/api/middlewares/auth/index.js
+++ Backend/src/api/middlewares/auth/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config;
 const jwt = require('jsonwebtoken');
+const messages = require('../../global/messages');
 const modelUsers = require('../../models/users');
 
 const jwtConfig = {
@@ -11,3 +12,11 @@ const createToken = (body) => {
   const token = jwt.sign({ data: body }, process.env.SECRET, jwtConfig);
   return token;
 };
+
+const verifyToken = async (req, res, next) => {
+  try {
+    
+  } catch (err) {
+    next()
+  }
+}
