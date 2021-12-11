6f3fce0f1760979dc0a64429e706ec2e7b9f7495
Elias Forte <eliasforte.dev@gmail.com>
2021-12-11 11:04:24 -0300

    adicionando middleware de erro no app.js

--- Backend/src/api/app.js
+++ Backend/src/api/app.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const root = require('./controllers/root');
+const error = require('./middlewares/error');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(root);
+app.use(error);
 
 module.exports = app;
