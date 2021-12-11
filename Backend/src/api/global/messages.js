297ba21faf72c2000a8ec35ef69cf63db8db0049
Elias Forte <eliasforte.dev@gmail.com>
2021-12-11 14:25:56 -0300

    criando novas mensagens de error

--- Backend/src/api/global/messages.js
+++ Backend/src/api/global/messages.js
@@ -1,11 +1,15 @@
 const { UNAUTHORIZED, CONFLICT } = require('http-status-codes').StatusCodes;
 
-const JWT_MALFORMED_401 = { message: 'jwt malformed', status: UNAUTHORIZED };
-const MISSING_TOKEN_401 = { message: 'missing auth token', status: UNAUTHORIZED };
+const JWT_MALFORMED_401 = { message: 'Jwt malformed', status: UNAUTHORIZED };
+const MISSING_TOKEN_401 = { message: 'Missing auth token', status: UNAUTHORIZED };
 const EMAIL_EXIST_409 = { message: 'Email already registered', status: CONFLICT };
+const INCORRECT_401 = { message: 'Incorrect username or password', status: 401 };
+const NOT_FOUND_404 = { message: 'User not exist', status: 404 };
 
 module.exports = {
   JWT_MALFORMED_401,
   MISSING_TOKEN_401,
-  EMAIL_EXIST_409
+  EMAIL_EXIST_409,
+  INCORRECT_401,
+  NOT_FOUND_404,
 };
