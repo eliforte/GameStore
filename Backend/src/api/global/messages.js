55ba1a7d9f48c6c50d0ecaa9b101848506e7aa74
Elias Forte <eliasforte.dev@gmail.com>
2021-12-11 15:24:03 -0300

    removendo nova mensagem de error

--- Backend/src/api/global/messages.js
+++ Backend/src/api/global/messages.js
@@ -5,7 +5,6 @@ const MISSING_TOKEN_401 = { message: 'Missing auth token', status: UNAUTHORIZED 
 const EMAIL_EXIST_409 = { message: 'Email already registered', status: CONFLICT };
 const INCORRECT_401 = { message: 'Incorrect username or password', status: 401 };
 const USER_NOT_EXIST_404 = { message: 'User not exist', status: 404 };
-const ALL_FIELDS_401 = { message: 'All fields must be filled', status: 401 };
 
 module.exports = {
   JWT_MALFORMED_401,
@@ -13,5 +12,4 @@ module.exports = {
   EMAIL_EXIST_409,
   INCORRECT_401,
   USER_NOT_EXIST_404,
-  ALL_FIELDS_401,
 };
