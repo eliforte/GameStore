bb9face01b521035497eb299be88c7f838ea1e12
Elias Forte <eliasforte.dev@gmail.com>
2021-12-11 15:28:44 -0300

    correção no middleware de validação do login

--- Backend/src/api/middlewares/validation/login.js
+++ Backend/src/api/middlewares/validation/login.js
@@ -1,13 +1,14 @@
 const Joi = require('joi');
 const rescue = require('express-rescue');
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
 
 const SCHEMA = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
 
-module.exports = rescue(async (req, res, next) => {
+module.exports = rescue(async (req, _res, next) => {
   const { error } = SCHEMA.validate(req.body);
-  if (error) return next();
+  if (error) return next({ message: error.message, status: BAD_REQUEST });
   next();
 });
