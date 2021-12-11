6202ef5214470a601c76a8c2ad9f9711297c93c9
Elias Forte <eliasforte.dev@gmail.com>
2021-12-11 10:50:15 -0300

    adicionando rota de registro no arquivo app.js

--- Backend/src/api/app.js
+++ Backend/src/api/app.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const root = require('./controllers/root');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(root);
 
 module.exports = app;
