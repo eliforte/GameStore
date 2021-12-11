f280401aba46574e9e99fab390c14f4935e2b984
Elias Forte <eliasforte.dev@gmail.com>
2021-12-11 16:53:49 -0300

    criada validação do campo resgiter dos users e games

--- Backend/src/api/controllers/root.js
+++ Backend/src/api/controllers/root.js
@@ -2,12 +2,12 @@ const express = require('express');
 const routerRegister = require('./users/register/router');
 const routerGames = require('./games/router');
 const routerLogin = require('./users/login/router')
-const validationLogin = require('../middlewares/validation/login');
+const validation = require('../middlewares/validation');
 
 const root = express.Router({ mergeParams: true });
 
-root.use('/register', routerRegister);
-root.use('/login', validationLogin, routerLogin);
+root.use('/register', validation.register, routerRegister);
+root.use('/login', validation.login, routerLogin);
 root.use('/register-game', routerGames);
 
 module.exports = root;
---
+++ Backend/src/api/middlewares/validation/index.js
@@ -0,0 +1,44 @@
+const Joi = require('joi');
+const rescue = require('express-rescue');
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const SCHEMALogin = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const SCHEMARegister = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  repeatPassword: Joi.ref('password'),
+});
+
+const SCHEMAGame = Joi.object({
+  name: Joi.string().required(),
+  price: Joi.string().required(),
+  quantity: Joi.number().required(),
+});
+
+const login = rescue(async (req, _res, next) => {
+  const { error } = SCHEMALogin.validate(req.body);
+  if (error) return next({ message: error.message, status: BAD_REQUEST });
+  next();
+});
+
+const register = rescue(async (req, _res, next) => {
+  const { error } = SCHEMARegister.validate(req.body);
+  if (error) return next({ message: error.message, status: BAD_REQUEST });
+  next();
+});
+
+const game = rescue(async (req, _res, next) => {
+  const { error } = SCHEMAGame.validate(req.body);
+  if (error) return next({ message: error.message, status: BAD_REQUEST });
+  next();
+});
+
+module.exports = {
+  login,
+  register,
+  game,
+};
