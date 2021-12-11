2615ede5d8488b6aee307ccb6be9f747f9d84f6f
Elias Forte <eliasforte.dev@gmail.com>
2021-12-11 16:02:08 -0300

    criada função createGame do service dos games

--- Backend/src/api/services/games/index.js
+++ Backend/src/api/services/games/index.js
@@ -1,2 +1,12 @@
 const modelGame = require('../../models/games');
 const messages = require('../../global/messages');
+
+const createGame = async ({ name, price, quantity, image }, infoUser) => {
+  const { _id: userId } = infoUser;
+  const newGame = await modelGame.create({ name, price, quantity, image, userId });
+  return newGame;
+};
+
+module.exports = {
+  createGame,
+};
