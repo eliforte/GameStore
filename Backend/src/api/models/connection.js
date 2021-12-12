3839115eb22dfb49ab73f934e0acba9617959fa7
Elias Forte <eliasforte.dev@gmail.com>
2021-12-12 19:20:32 -0300

    atualizando url de conecção com o banco de dados

--- Backend/src/api/models/connection.js
+++ Backend/src/api/models/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/GameStore`;
+const MONGO_DB_URL = `mongodb+srv://${process.env.USER}:${process.env.SECRET_MONGODB}@cluster0.oksz0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
