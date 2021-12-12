c51847c8f474c73c495ce35133eea106ded6c076
Elias Forte <eliasforte.dev@gmail.com>
2021-12-12 15:48:21 -0300

    adicionando rotas dos controlles dos games

--- Backend/src/api/controllers/games/router.js
+++ Backend/src/api/controllers/games/router.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const create = require('./create');
 const findById = require('./findById');
+const list = require('./list');
+const remove = require('./remove');
+const update = require('./update');
 const validade = require('../../middlewares/validation');
 
 const router = express.Router({ mergeParams: true });
 
+router.get('/', list);
 router.post('/register',validade.gameRegister, create);
-router.get('/:id', findById)
+router.get('/:id', findById);
+router.put('/:id',validade.gameRegister, update);
+router.delete('/:id', remove);
 
 module.exports = router;
