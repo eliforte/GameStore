1510375557d1bd323391315456ec3d06b8306c19
Elias Forte <eliasforte.dev@gmail.com>
2021-12-14 00:34:50 -0300

    adicionando mensagem de retorno de upload de arquivo

--- Backend/src/api/controllers/upload/post.js
+++ Backend/src/api/controllers/upload/post.js
@@ -10,5 +10,5 @@ module.exports = rescue(async (req, res, next) => {
   if (newGame.message) return next(newGame)
   console.log(req.file);
 
-  return res.status(OK).json()
+  return res.status(OK).json({ message: 'Upload concluído com sucesso!' })
 });
