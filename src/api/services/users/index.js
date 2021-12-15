d62caff8773e8f6e68b0fe905cb30352482bde33
Elias Forte <eliasforte.dev@gmail.com>
2021-12-15 17:32:43 -0300

    correção no retorno dos erros do service de users

--- src/api/services/users/index.js
+++ src/api/services/users/index.js
@@ -1,19 +1,20 @@
 const modelUser = require('../../models/users');
+const { ApiError: { NewError } } = require('../../global/error/apiError')
 const auth = require('../../global/middlewares/auth');
 const messages = require('../../global/error/messages');
 
 const createUser = async ({ email, password, repeatPassword }) => {
   const userExist = await modelUser.findByEmail(email);
-  if (userExist) return messages.EMAIL_EXIST_409;
+  if (userExist) return NewError(messages.EMAIL_EXIST_409);
 
   return modelUser.create({ email, password, repeatPassword });
 };
 
 const loginUser = async ({ email, password }) => {
   const userExist = await modelUser.findByEmail(email);
-  if (!userExist) return messages.USER_NOT_EXIST_404;
+  if (!userExist) return NewError(messages.USER_NOT_EXIST_404);
   if (userExist.email !== email || userExist.password !== password) {
-    return messages.INCORRECT_401;
+    return NewError(messages.INCORRECT_401);
   };
 
   const { password: passwordDB, repeatPassword: repeatDB, ...userWithoutPassword } = userExist;
