477dc36e8167d6f784fe55aa25836a10f28fb817
Elias Forte <eliasforte.dev@gmail.com>
2021-12-19 23:54:03 -0300

    correção na desconstrução no controller de compras

--- src/api/controllers/games/updateDB.js
+++ src/api/controllers/games/updateDB.js
@@ -3,9 +3,10 @@ const service = require('../../services/games');
 const { OK } = require('http-status-codes').StatusCodes;
 
 module.exports = rescue(async (req, res, _next) => {
-  const { purshare } = req.body;
+  const { purchase } = req.body;
+  console.log(purchase);
   await Promise.all(
-    purshare.map((game) => {
+    purchase.map((game) => {
       return service.updateGame(game._id, game);
     })
   );
